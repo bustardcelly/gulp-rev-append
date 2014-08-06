54ed4d1a5c55cef0cb6f11c57fc908ad57cd46b6
Matt Rubin <opensource@mattrubin.me>
2014-08-06 15:49:09 -0700

    Add support for url() references in CSS files

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var map = require('event-stream').map;
 
-var FILE_DECL = /(?:href|src)="(.*)[\?]rev=(.*)[\"]/gi;
+var FILE_DECL = /(?:href="|src="|url\()(.*)[\?]rev=(.*)[\"\)]/gi;
 
 var revPlugin = function revPlugin() {
 
